e4c387d0d07495a983c358acb38ee38a0c55ba3d
Nisha Bankar <nishab2607@gmail.com>
2025-05-09 16:26:29 +0530

    Delete travel story api route

--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { addTravelStory, getAllTravelStory, imageUpload, deleteImage, editTravelStory } from "../controllers/travelStory.controller.js"
+import { addTravelStory, getAllTravelStory, imageUpload, deleteImage, editTravelStory, deleteTravelStory } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 
@@ -10,6 +10,7 @@ router.post( "/image-upload", upload.single( "image" ), imageUpload )
 router.delete("/delete-image", deleteImage)
 router.post( "/add", verifyToken, addTravelStory )
 router.get( "/get-all", verifyToken, getAllTravelStory )
-router.post("/edit-story/:id", verifyToken, editTravelStory)
+router.post( "/edit-story/:id", verifyToken, editTravelStory )
+router.delete("/delete-story/:id", verifyToken, deleteTravelStory)
 
 export default router
